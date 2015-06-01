3865f701ddface485e8b662cfa8b8dd4ad486ba1
JiorMajor <jiormajor@gmail.com>
2015-06-01 23:10:28 +0800

    BookingRegister Class
    A class which will keep lists of Booking objects indexed by Facility.
    Thats mean I will use HashMap<Facility, ArrayList<Booking>>.

---
+++ BookingRegister.java
@@ -0,0 +1,15 @@
+package workshop;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class BookingRegister {
+	
+	private HashMap<Facility, ArrayList<Booking>> bookRegisterhm;
+
+	public BookingRegister(HashMap<Facility, ArrayList<Booking>> bookRegisterhm) {
+		this.bookRegisterhm = bookRegisterhm;
+	}
+
+	
+}
